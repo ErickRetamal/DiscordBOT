b8cf1b7ace60b6a987ba02ca4981ea9e157f0acb
Erick Marcelo Retamal Cornejo <111723306+ErickRetamal@users.noreply.github.com>
2025-01-13 19:41:22 -0300

    Update Discord.py

--- Discord.py
+++ Discord.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import tasks, commands
 from riotwatcher import LolWatcher
 import os
+from discord import Interaction
+from discord.commands import slash_command 
 
 # Configuración de API y Token
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
